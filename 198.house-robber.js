9091f33c3da10b52168bfb001c19d862584e5c57
xujun1 <xuj2017@outlook.com>
2019-07-29 19:58:36 +0800

    add

--- 198.house-robber.js
+++ 198.house-robber.js
@@ -7,28 +7,45 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+
 var rob = function(nums) {
-    if(nums.length === 0){
+    // if(nums.length === 0){
+    //     return 0
+    // }
+
+    // if(nums.length === 1){
+    //     return nums[0]
+    // }
+
+    // let sum1 = nums[0];
+    // let sum2 = nums[1];
+
+    // for(let lastIndex = 2;lastIndex<nums.length;lastIndex ++){
+    //     let temp = sum1;
+    //     if(sum2 > sum1){
+    //         sum1 = sum2;
+    //     }
+
+    //     sum2 = temp + nums[lastIndex]
+    // }
+
+    // return sum1 > sum2 ? sum1 : sum2
+
+    let len = nums.length;
+    if(len == 0){
         return 0
     }
-
-    if(nums.length === 1){
+    if(len == 1){
         return nums[0]
     }
-
-    let sum1 = nums[0];
-    let sum2 = nums[1];
-
-    for(let lastIndex = 2;lastIndex<nums.length;lastIndex ++){
-        let temp = sum1;
-        if(sum2 > sum1){
-            sum1 = sum2;
-        }
-
-        sum2 = temp + nums[lastIndex]
+    let dp = [];
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0],nums[1])
+    for(let i = 2;i<nums.length;i++){
+        dp[i]= Math.max(dp[i-2]+nums[i],dp[i-1])
     }
-
-    return sum1 > sum2 ? sum1 : sum2
+    return dp[len-1]
 
 };
 
