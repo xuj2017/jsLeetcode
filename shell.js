2d1ad821198f95a2c6f97cb853f33822dbef1d4c
xujun1 <xuj2017@outlook.com>
2019-07-30 17:11:56 +0800

    add shell

--- shell.js
+++ shell.js
@@ -1,8 +1,7 @@
 let exec = require('child_process').exec;
 let option = process.argv.splice(2);
-let message = option.length ? option : "add";
-var command =  `git add . & git commit -m ${message} & git push origin master`;
-
+let message = option.length ? option.join(" ") : "add";
+var command =  `git add . & git commit -m "${message}" & git push origin master`;
 exec(command,(error, stdout, stderr) =>{
     if (error) {
         console.error(`执行的错误: ${error}`);
